428e68a8967ae2d63c63a6a9a0a3dc8e49eda0c7
mustafa <mustafa@linux-x47y>
2016-12-23 14:36:47 +0530

    __init__ with logging

---
+++ python/predict_sales/__init__.py
@@ -0,0 +1,20 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+logger.setLevel(logging.DEBUG)
+
+fh = logging.FileHandler('../rossman.log')
+fh.setLevel(logging.DEBUG)
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+fh_formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+ch_formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+
+fh.setFormatter(fh_formatter)
+ch.setFormatter(ch_formatter)
+
+logger.addHandler(ch)
+logger.addHandler(fh)
