631f3fae0eca48fdcbe7000aadff1a6b8d7004f0
mustafa <mustafa@linux-x47y>
2016-12-27 22:06:08 +0530

    Fixed select_features.
    Predictions namedtuple now defined locally in functions.
    Elasticnet predictions and computation of cross val score.

--- python/predict_sales/functions.py
+++ python/predict_sales/functions.py
@@ -3,9 +3,9 @@ from collections import namedtuple
 import numpy as np
 import pandas as pd
 
-from sklearn import linear_model
+from sklearn import linear_model, model_selection
 
-from .data import log_lm_features, check_nulls
+from .data import log_lm_features, check_nulls, make_fold
 import logging
 
 logger = logging.getLogger(__name__)
@@ -72,7 +72,8 @@ def log_transform_train(train):
 
 @allow_modifications(False)
 def select_features(train, features):
-    columns_to_select = ['Sales', 'Id', 'Store', 'Date'] + list(features)
+    base_columns = [c for c in ['Sales', 'Id', 'Store', 'Date'] if c in train.columns]
+    columns_to_select = base_columns + list(features)
     return train[columns_to_select]
 
 
@@ -97,12 +98,11 @@ def remove_outliers_lm(train, features=log_lm_features,
     return pd.concat(gen)
 
 
-Predictions = namedtuple('Predictions', 'predicted, fit, store')
-
-
 @allow_modifications(False)
 def predict_lm(train, test, save_fit=False):
     logger.info("{f} train shape {tr}, test shape {te}".format(f=predict_lm.__name__, tr=train.shape, te=test.shape))
+    Predictions = namedtuple('Predictions', 'predicted, fit, store')
+    logger.info("Prediction fields {0}".format(Predictions._fields))
 
     @allow_modifications(True)
     def predict_lm_per_store(store_train, store_test, save_fit=save_fit):
@@ -121,12 +121,92 @@ def predict_lm(train, test, save_fit=False):
     return Predictions(*zip(*predict_per_store(train, test, predict_lm_per_store)))
 
 
+def glmnet(X, y, family, alpha, nlambda):
+    return False
+
+
 @allow_modifications(False)
-def predict_glmnet(train, test, eval_function, steps=13,
-                   predict_interval=6 * 7, step_by=7,
-                   alpha=1, family=("gaussian", "poisson"),
-                   nlambda=100):
-    pass
+def best_glmnet_alpha(store_train, eval_function, steps, predict_interval, step_by, l1_ratio, family, nlambda):
+    logger.info("{f} store train shape {tr}".format(f=best_glmnet_alpha.__name__, tr=store_train.shape))
+    Predictions = namedtuple('Predictions', 'predictions, actual, scores, fit')
+    logger.info("Prediction fields {0}".format(Predictions._fields))
+
+    global_fit = glmnet(store_train.drop(['Sales', 'Date'], axis=1),
+                        store_train['Sales'], family=family, alpha=l1_ratio, nlambda=nlambda)
+    lambdas = global_fit.glmnet_lambda
+
+    def per_step_map(step):
+        fold = make_fold(store_train, step, predict_interval, step_by)
+        fold_fit = glmnet(fold.train.drop(['Sales', 'Date'], axis=1), fold.train['Sales'],
+                          family=family, alpha=l1_ratio, lambdas=lambdas)
+        pred = fold_fit.predict(fold.test.drop(['Id', 'Date'], axis=1), type="response")
+
+        # if (nrow(fold$test) < 2) {
+        # predictions < - matrix(0, nrow = nrow(predictions), ncol = ncol(predictions))
+        # }
+
+        scores = eval_function(pred, fold.actual['Sales'])
+
+        return Predictions(predictions=pred, actual=fold.actual['Sales'],
+                           scores=scores, fit=fold_fit)
+
+
+def cv_generator(train, steps, predict_interval, step_by):
+    for step in range(1, steps + 1):
+        yield make_fold(train=train, step=step,
+                        predict_interval=predict_interval, step_by=step_by)
+
+
+@allow_modifications(False)
+def predict_elasticnet(train, test=None, with_cv=False, eval_function=None, steps=13,
+                       predict_interval=6 * 7, step_by=7,
+                       l1_ratio=[ .99, 1], family="gaussian",
+                       n_alphas=100):
+    logger.info("{f} train shape {tr}, test shape {te}"
+                .format(f=predict_elasticnet.__name__, tr=train.shape, te=test.shape))
+    Predictions = namedtuple('Predictions', 'predicted, fit, errors, store')
+    logger.info("Prediction fields {0}".format(Predictions._fields))
+    assert family in ("gaussian", "poisson")
+
+    @allow_modifications(True)
+    def predict_elasticnet_per_store(store_train, store_test):
+        store_train = store_train.reset_index(drop=True)
+        cv = list(cv_generator(store_train, steps, predict_interval=predict_interval, step_by=step_by))
+        en = linear_model.ElasticNetCV(l1_ratio=l1_ratio, n_alphas=n_alphas, cv=cv)
+
+        sales = store_train['Sales']
+        without_date_sales = store_train.drop(['Date', 'Sales'], axis=1)
+        fit = en.fit(without_date_sales, sales)
+        pred = fit.predict(store_test[list(without_date_sales.columns)])
+        store_test['PredictedSales'] = pred
+
+        if with_cv:
+            cv_errors = []
+            for fold in cv:
+                cv_en = linear_model.ElasticNet(alpha=fit.alpha_, l1_ratio=fit.l1_ratio_)
+                cv_train = without_date_sales.iloc[fold.train_idx, :]
+                cv_train_sales = sales[fold.train_idx]
+                cv_fit = cv_en.fit(cv_train, cv_train_sales)
+                cv_test = without_date_sales.iloc[fold.test_idx, :]
+                cv_test_sales = sales[fold.test_idx]
+                cv_pred = cv_fit.predict(cv_test)
+                cv_error = rmspe(np.exp(cv_pred) * cv_test['Open'], np.exp(cv_test_sales))
+                cv_errors.append(cv_error)
+
+            cv_median_error = np.median(cv_errors)
+            logger.info('Store {0}. CV errors {1}'.format(store_train['Store'][0], cv_errors))
+            logger.info('CV median error {0}'.format(cv_median_error))
+
+
+        # logger.info('alpha: {alpha}, l1 ratio: {l1_ratio}'
+        #             .format(alpha=fit.alpha_, l1_ratio=fit.l1_ratio_))
+        # cv = list(cv_generator(store_train, steps, predict_interval=predict_interval, step_by=step_by))
+        # scores = model_selection.cross_val_score(fit, X=without_date_sales, y=sales,
+        #                                          scoring=None, cv=cv, n_jobs=1, pre_dispatch='n_jobs')
+        # logger.info('Median score: {0}'.format(scores.median()))
+        return Predictions(predicted=store_test, fit=fit, errors=cv_median_error, store=None)
+
+    return Predictions(*zip(*predict_per_store(train, test, predict_elasticnet_per_store)))
 
 
 def predict_per_store(train, test, predict_fun):
