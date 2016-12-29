4695879b0cff40e3f4421c933610564b6cb56e76
mustafa <mustafa@linux-x47y>
2016-12-30 00:36:17 +0530

    Modifying dataframes in place
    Added parameter to turn off wrapping.

--- python/predict_sales/functions.py
+++ python/predict_sales/functions.py
@@ -1,3 +1,4 @@
+import functools
 from collections import namedtuple
 
 import numpy as np
@@ -13,11 +14,18 @@ import xgboost as xgb
 logger = logging.getLogger(__name__)
 logger.info("Module loaded")
 
+WRAP = False
+logger.info('Wrapping all functions to check df modifications' if WRAP else 'Wrapping disabled')
+
 
 def allow_modifications(allow):
     def decorate(func):
+        if not WRAP:
+            return func
+
         func_name = func.__name__
 
+        @functools.wraps(func)
         def wrapper(*args, **kwargs):
             dfs_with_copy = [(df, df.copy()) for df in args if isinstance(df, pd.DataFrame)]
             return_val = func(*args, **kwargs)
@@ -45,7 +53,7 @@ def allow_modifications(allow):
     return decorate
 
 
-@allow_modifications(False)
+@allow_modifications(True)
 def remove_before_changepoint(train):
     changepoints = {837: '2014-03-16',
                     700: '2014-01-03',
@@ -61,49 +69,67 @@ def remove_before_changepoint(train):
                     803: '2014-01-07',
                     91: '2014-01-14'}
     for store, date in changepoints.items():
-        train = train.query('Store != {store} or Date > "{date}"'.format(store=store, date=date))
-    return train
+        train.query('Store != {store} or Date > "{date}"'.format(store=store, date=date), inplace=True)
 
 
-@allow_modifications(False)
+@allow_modifications(True)
 def log_transform_train(train):
-    train = train.query('Sales > 0')
+    train.query('Sales > 0', inplace=True)
     train['Sales'] = np.log(train['Sales'])
-    return train
 
 
 @allow_modifications(False)
-def select_features(train, features):
+def select_features_copy(train, features):
     base_columns = [c for c in ['Sales', 'Id', 'Store', 'Date']
                     if c in train.columns and c not in features]
     columns_to_select = base_columns + list(features)
     return train[columns_to_select]
 
 
-@allow_modifications(False)
+@allow_modifications(True)
+def select_features_inplace(train, features):
+    base_columns = [c for c in ['Sales', 'Id', 'Store', 'Date']
+                    if c in train.columns and c not in features]
+    columns_to_select = base_columns + list(features)
+    columns_to_drop = set(train.columns).difference(columns_to_select)
+    train.drop(columns_to_drop, axis=1, inplace=True)
+
+
+def select_features(train, features, inplace=False):
+    if inplace:
+        select_features_inplace(train, features)
+    else:
+        return select_features_copy(train, features)
+
+
+@allow_modifications(True)
 def remove_outliers_lm(train, features=log_lm_features,
                        z_score=2.5):
     logger.info("{f} train shape {tr}".format(f=remove_outliers_lm.__name__, tr=train.shape))
-    if 'Id' not in train.columns:
-        logger.info('Adding Id to train data columns.')
+    # if 'Id' not in train.columns:
+    #     logger.info('Adding Id to train data columns.')
 
-    with_id = train.copy()
+    with_id = train
     with_id['Id'] = np.arange(1, train.shape[0] + 1)
-    with_fit = predict_lm(select_features(with_id, log_lm_features), with_id).predicted
+    with_fit = predict_lm(select_features(with_id, log_lm_features), None).predicted
 
     def filter_by_z_score():
         for store_prediction in with_fit:
             errors = abs(store_prediction['PredictedSales'] - store_prediction['Sales'])
             z_scores = errors / errors.median()
-            yield store_prediction[z_scores < z_score].drop('Id', axis=1)
+            yield z_scores < z_score
 
     gen = filter_by_z_score()
-    return pd.concat(gen)
+    return with_id.loc[pd.concat(gen),:].drop('Id', axis=1)
 
 
 @allow_modifications(False)
 def predict_lm(train, test, save_fit=False):
-    logger.info("{f} train shape {tr}, test shape {te}".format(f=predict_lm.__name__, tr=train.shape, te=test.shape))
+    if test is None:
+        logger.info("{f} train shape {tr}, no test".format(f=predict_lm.__name__, tr=train.shape))
+    else:
+        logger.info(
+            "{f} train shape {tr}, test shape {te}".format(f=predict_lm.__name__, tr=train.shape, te=test.shape))
     Predictions = namedtuple('Predictions', 'predicted, fit, store')
     logger.info("Prediction fields {0}".format(Predictions._fields))
 
@@ -114,8 +140,14 @@ def predict_lm(train, test, save_fit=False):
         store_train = store_train.drop(['Date', 'Sales'], axis=1)
         lm = linear_model.LinearRegression()
         fit = lm.fit(store_train, sales)
-        pred = lm.predict(store_test[list(store_train.columns)])
+        store_test_sales = None
+        if store_test is None:
+            store_test = store_train
+            store_test_sales = sales
+        pred = fit.predict(store_test[list(store_train.columns)])
         store_test['PredictedSales'] = pred
+        if store_test_sales is not None:
+            store_test['Sales'] = store_test_sales
         if save_fit:
             return Predictions(predicted=store_test, fit=fit, store=None)
         else:
@@ -169,35 +201,43 @@ def predict_elasticnet(train, test=None, with_cv=False, eval_function=None, step
                 .format(f=predict_elasticnet.__name__, tr=train.shape, te=test.shape))
     Predictions = namedtuple('Predictions', 'predicted, fit, errors, store')
     logger.info("Prediction fields {0}".format(Predictions._fields))
+    logger.info("Date in train: {0}".format('Date' in train.columns))
     assert family in ("gaussian", "poisson")
 
     @allow_modifications(True)
     def predict_elasticnet_per_store(store_train, store_test):
-        store_train = store_train.reset_index(drop=True)
+        store_train.reset_index(drop=True, inplace=True)
         cv = list(cv_generator(store_train, steps, predict_interval=predict_interval, step_by=step_by))
         en = linear_model.ElasticNetCV(l1_ratio=l1_ratio, n_alphas=n_alphas, cv=cv)
 
         sales = store_train['Sales']
-        without_date_sales = store_train.drop(['Date', 'Sales'], axis=1)
-        fit = en.fit(without_date_sales, sales)
-        pred = fit.predict(store_test[list(without_date_sales.columns)])
+        store = store_train['Store'].iloc[0]
+        store_train.drop(['Date', 'Sales'], axis=1, inplace=True)
+        store_test_id = store_test['Id']
+        store_test.drop(set(store_test.columns).difference(store_train.columns), axis=1, inplace=True)
+        fit = en.fit(store_train, sales)
+        pred = fit.predict(store_test)
+        store_test.drop(set(store_test.columns).difference('Id'), axis=1, inplace=True)
         store_test['PredictedSales'] = pred
+        # store_test = pd.concat([store_test_id, pd.Series(pred, name='PredictedSales')], axis=1)
+        # # store_test.columns = ['Id', 'PredictedSales']
 
+        cv_median_error = None
         if with_cv:
             cv_errors = []
             for fold in cv:
                 cv_en = linear_model.ElasticNet(alpha=fit.alpha_, l1_ratio=fit.l1_ratio_)
-                cv_train = without_date_sales.iloc[fold.train_idx, :]
+                cv_train = store_train.iloc[fold.train_idx, :]
                 cv_train_sales = sales[fold.train_idx]
                 cv_fit = cv_en.fit(cv_train, cv_train_sales)
-                cv_test = without_date_sales.iloc[fold.test_idx, :]
+                cv_test = store_train.iloc[fold.test_idx, :]
                 cv_test_sales = sales[fold.test_idx]
                 cv_pred = cv_fit.predict(cv_test)
                 cv_error = rmspe(np.exp(cv_pred) * cv_test['Open'], np.exp(cv_test_sales))
                 cv_errors.append(cv_error)
 
             cv_median_error = np.median(cv_errors)
-            logger.info('Store {0}. CV errors {1}'.format(store_train['Store'][0], cv_errors))
+            logger.debug('Store {0}. CV errors {1}'.format(store, cv_errors))
             logger.info('CV median error {0}'.format(cv_median_error))
 
         # logger.info('alpha: {alpha}, l1 ratio: {l1_ratio}'
@@ -238,16 +278,21 @@ def predict_xgboost(train, test, eval_function, params, nrounds):
 
 def predict_per_store(train, test, predict_fun):
     train_gb = train.groupby('Store')
-    test_gb = test.groupby('Store')
+    test_gb = None
+    if test is not None:
+        test_gb = test.groupby('Store')
 
     for s, store_train in train_gb:
-        store_test = test_gb.get_group(s)
+        store_test = None
+        if test is not None:
+            store_test = test_gb.get_group(s)
+
         yield predict_fun(store_train, store_test)._replace(store=s)
 
 
 @allow_modifications(True)
-def log_revert_predicted(predicted):
-    predicted['PredictedSales'] = np.exp(predicted['PredictedSales']) * predicted['Open']
+def log_revert_predicted(predicted, open):
+    predicted['PredictedSales'] = np.exp(predicted['PredictedSales']) * open
 
 
 def rmspe(predicted, actual):
